8ee8fe06f4bfb04bd1b5e6e489307c1450ead1c5
Karim <ahadovkarim@gmail.com>
2022-10-04 21:49:34 +0300

    initial

---
+++ js/lang.js
@@ -0,0 +1,91 @@
+const langArr = {
+    "clubname" : {
+        "ru": "«ЯСобой»",
+        "uz": "«Men O'zimman»"
+    },
+    "Iam": {
+        "ru":"Я Гулрух Шакировна",
+         "uz": "Men Gulrux Shakirovna"
+    },
+    "homedescrib": {
+        "ru":"﻿Это пространство единомышленниц, где мы начинаем осознавать свои истинные желания, любить и принимать себя.",
+        "uz":"Bu biz o'zimizning haqiqiy istaklarimizni ro'yobga chiqarishni, o'zimizni sevishni va o'zimizni qabul qilishni boshlaydigan hamfikrlar maydonidir.",
+    },
+    "xochu": {
+        "ru":" Хочу в клуб «ЯСобой»!",
+        "uz":"«ЯСобой»! klubiga azo!",
+    },
+    "scroll": {
+        "ru":"Вниз",
+        "uz":"Pastga",
+    },
+    "lng-home": {
+        "ru":"Главная",
+        "uz":"Asosiy",
+    },
+    "lng-autor": {
+        "ru":"Об авторе",
+        "uz":"Muallif ",
+    },
+    "club-name-nav": {
+        "ru":"ЯСобой",
+        "uz":"ЯСобой",
+    },
+    "lng-contact": {
+        "ru":"Контакты",
+        "uz":"Kontaktlar",
+    },
+    "lng-club-about": {
+        "ru":" . ",
+        "uz":"",
+    },
+    "lng-club-about2": {
+        "ru":"",
+        "uz":"",
+    },
+    "lng-club-descsr": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+    "home_describ": {
+        "ru":"",
+        "uz":"",
+    },
+
+    
+
+    
+
+}
